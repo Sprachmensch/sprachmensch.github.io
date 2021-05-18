9a5cebcfd78537047664230c163b4baa5e4af3bf
Sprachmensch <41573839+Sprachmensch@users.noreply.github.com>
2021-05-18 19:19:11 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 import markdown2
-tmp= markdown2.markdown("*-markdown2 up and running!*")
+tmp= markdown2.markdown("**-markdown2 up and running!**")
 
 file=open("index.html","r")
 myData= file.read().replace("</body>",f"{tmp}\n</body>")
