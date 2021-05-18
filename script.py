72f89c02f3b8a1f29b03cf46d0ef62d7541cd478
Sprachmensch <41573839+Sprachmensch@users.noreply.github.com>
2021-05-18 19:13:42 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,8 @@
+import markdown2
+tmp= markdown2.markdown("*-markdown2 up and running!*")
+
 file=open("index.html","r")
-myData= file.read().replace("</body>","<p>Oh a new Line, added via GitHub actions</p>\n</body>")
+myData= file.read().replace("</body>",f"{tmp}\n</body>")
 file.close()
 
 file=open("index.html","w")
