b8302c4364f1fedde07db322d495f61c31eff47e
Sprachmensch <41573839+Sprachmensch@users.noreply.github.com>
2021-05-18 18:51:30 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
-file=open("index.html","r+")
-tmp= file.read()
-myData=tmp.replace("</body>","<p>Oh a new Line, added via GitHub actions</p></body>")
+file=open("index.html","r")
+myData= file.read().replace("</body>","<p>Oh a new Line, added via GitHub actions</p>\n</body>")
+file.close()
+
+file=open("index.html","w")
 file.write(myData)
 file.close()
 print("Done!")
