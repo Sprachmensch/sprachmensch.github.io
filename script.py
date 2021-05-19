8049984f9a22edc3deef691629dfa49f153e5973
sprachmensch <sprachmensch@gmx.de>
2021-05-19 13:23:01 +0200

    update

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import markdown2
+import datetime
 
 header = """<!DOCTYPE html>
 <html lang="en">
@@ -8,11 +9,13 @@ header = """<!DOCTYPE html>
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <title>My Github actions Testsite</title>
 </head><body>"""
-footer = '</body>'
+date=datetime.datetime.today()
+footer = f'<br><p>Last updated on: {date}</p></body>'
 
 file = open("index.html", "w")
 file.write(header)
 file.write(markdown2.markdown_path("src/index.md"))
+
 file.write(footer)
 file.close()
 print("Done!")
