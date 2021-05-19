6321e405f9eb3572df1fdff335fd93a0a2f20658
Sprachmensch <41573839+Sprachmensch@users.noreply.github.com>
2021-05-19 13:14:41 +0200

    Update script.py

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ header = """<!DOCTYPE html>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title></title>
+<title>My Github actions Testsite</title>
 </head><body>"""
 footer = '</body>'
 
